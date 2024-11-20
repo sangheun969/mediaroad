7f94ccaad6f8913f1cb30923c27eaa9281c0640f
sangheun969 <sang969@naver.com>
2024-11-20 16:35:25 +0900

    fix:next config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -3,8 +3,8 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
-  assetPrefix: isProd ? "/mediaroad/" : "",
   basePath: isProd ? "/mediaroad" : "",
+  assetPrefix: isProd ? "/mediaroad" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
