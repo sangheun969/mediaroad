fd70b00a0be71fab181ff07a991c9d81e957c6e5
sangheun969 <sang969@naver.com>
2024-11-24 17:34:20 +0900

    fix: global css

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   basePath: isProd ? "/mediaroad" : "",
-  assetPrefix: isProd ? "/mediaroad" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
